16da41ed3bb0e57eafcd4e16a5e93e76588ba6e6
Brad Main <bmain@ucdavis.edu>
2020-06-23 15:18:03 -0700

    cut_script

---
+++ check_cut_sites.py
@@ -0,0 +1,30 @@
+# This python script prints out the number of sequence matches in our oligo library.
+# This was used to confirm that PEPSYN software worked properly.
+# At these are palidromic sequences, the rev. complement is the same.
+2020-06-23
+bmain 
+
+hind='AAGCTT'
+EcoR1='GAATTC'
+
+input = "protien_seqs_sorted.tsv"
+
+def cut_site_finder(oligo_file,site):
+    D={}
+    for line in open(oligo_file):
+        i=line.strip().split()
+        oligo = i[1]
+        cut_count = oligo.count(site)
+        if cut_count not in D:
+            D[cut_count]=0
+        D[cut_count]+=1
+    return(D)
+
+HindIII_cuts = cut_site_finder(input,hind)
+for c in HindIII_cuts:
+    print("HindIII",c,HindIII_cuts[c])
+
+EcoR1_cuts = cut_site_finder(input,EcoR1)
+for count in EcoR1_cuts:
+    print("EcoR1",count,EcoR1_cuts[count])
+
